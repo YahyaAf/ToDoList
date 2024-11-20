9890a7dbacae22f2041da436bbe104cd6ec762c6
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-11-20 15:03:19 +0100

    att Dark mode de mon projet

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -394,10 +394,20 @@ verifierSubmit = () => {
     }
 
 }
+
+const darkModeToggle = document.getElementById("darkModeToggle");
+if (localStorage.getItem("dark-mode") === "enabled") {
+    document.body.classList.add("dark-mode");
+}
+darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("dark-mode", "enabled");
+    } else {
+        localStorage.removeItem("dark-mode");
+    }
+});
+
 verifierSubmit();
-
-
-
- 
 
 ReadAll(); 
